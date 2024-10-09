ea8cff986123bd463d13dae95808db3f95e527da
gd8681 <162702571+gd8681@users.noreply.github.com>
2024-10-09 09:26:14 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Historia',
+    'Quem causou o incidente de 11 de setembro?',
+    'Celso Portiolli'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a nação mais nova do mundo?',
+    'Sudão do Sul'
+)
+
+criaCartao(
+    'Programação',
+    'Qual a utilidade do GitHub?',
+    'É uma plataforma amplamente utilizada para hospedagem de códico-fonte e a colaboração em projetos de software'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz peixe bola gato em ingles?',
+    'Fish ball cat'
+)
